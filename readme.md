180c04f7e62a273b9108f9b08502f5a146deba89
Pradeep <pradeep@tor.us>
2023-03-21 17:23:46 +0530

    initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Sample dApp
+Steps to run:
+1. use any server like go live
+2. run and explore
---
+++ script.js
@@ -0,0 +1,54 @@
+const MoodContractABI = [
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_mood",
+        type: "string",
+      },
+    ],
+    name: "setMood",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getMood",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+const MoodContractAddress = "0xeC70FC4AAc56c45B4548D250e9669C8150396f9A";
+let MoodContract;
+let signer;
+const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli");
+provider.send("eth_requestAccounts", []).then(() => {
+  provider.listAccounts().then((accounts) => {
+    signer = provider.getSigner(accounts[0]);
+    MoodContract = new ethers.Contract(
+      MoodContractAddress,
+      MoodContractABI,
+      signer
+    );
+  });
+});
+async function getMood() {
+  const getMoodPromise = MoodContract.getMood();
+  const Mood = await getMoodPromise;
+  document.getElementById("showMood").innerText = `Your Mood: ${Mood}`;
+  console.log(Mood);
+}
+
+async function setMood() {
+  const mood = document.getElementById("mood").value;
+  const setMoodPromise = MoodContract.setMood(mood);
+  await setMoodPromise;
+}
